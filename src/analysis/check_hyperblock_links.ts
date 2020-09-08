c5960e370ea6bf09ff93b48f4032d2b8efb752bf
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-09-08 12:02:34 +0300

    Improve, refactor.

--- src/analysis/check_hyperblock_links.ts
+++ src/analysis/check_hyperblock_links.ts
@@ -16,7 +16,7 @@ async function main() {
     let folders = readdirSync(workspace, { withFileTypes: true }).filter((item: any) => item.isDirectory());
 
     for (const [index, item] of folders.entries()) {
-        console.log("Folder:", item.name);
+        console.log(">>> Folder:", item.name);
 
         let nodePath = path.join(workspace, item.name);
         let shard = shardAssignments[index];
@@ -38,53 +38,46 @@ async function main() {
 
 async function doCheck(nodeDb: NodeDatabase) {
     let hashes = await nodeDb.getLookupTxHashes();
-
-    let rewardsIssues = 0;
-    let scrIssues = 0;
-    let regularIssues = 0;
+    let issues = 0;
 
     for (const hash of hashes) {
         let miniblockMetadata = await nodeDb.getMiniblockMetadataByTx(hash);
-        let isRewards = miniblockMetadata!.isRewards();
-        let isSCR = miniblockMetadata!.isSCR();
         let metablockHash = miniblockMetadata!.hyperblockHash();
         let metaBlock = await nodeDb.getMetaBlock(metablockHash);
 
         if (!metaBlock) {
-            if (isRewards) {
-                rewardsIssues++;
-                continue;
-            }
+            issues++;
 
-            if (isSCR) {
-                scrIssues++;
-                continue;
-            }
-
-            regularIssues++;
-
-            console.log("Bad link");
+            console.log("=================================")
+            console.log("! Bad link, metablock not found.");
+            console.log("=================================")
+            console.log("! Miniblock", miniblockMetadata?.hash);
+            console.log("! Metablock", metablockHash);
+            console.log("! Tx", hash);
             console.log("=================================")
             console.log(miniblockMetadata);
-            console.log("Miniblock", miniblockMetadata?.hash);
-            console.log("Metablock", metablockHash);
-            console.log("Transaction", hash);
+            console.log("=================================")
+
             continue;
         }
 
         let contains = metaBlock.containsMiniblock(miniblockMetadata!.hash);
         if (!contains) {
+            issues++;
+
+            console.log("=================================")
             console.log(`! Bad link, miniblock ${miniblockMetadata!.hash} (${miniblockMetadata!.type}) not found in metablock ${metaBlock.nonce}`);
             console.log(`! Tx ${hash}`);
+            console.log("=================================")
             console.log(miniblockMetadata);
+            console.log("=================================")
             console.log(metaBlock);
+            console.log("=================================")
         }
     }
 
-    console.log("\ttotal txs", hashes.length);
-    console.log("\tregular tx issues", regularIssues);
-    console.log("\trewards issues", rewardsIssues);
-    console.log("\tscr issues", scrIssues);
+    console.log("total txs", hashes.length);
+    console.log("issues", issues);
 }
 
 (async () => {
