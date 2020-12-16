5abe8340cee4e1037a573d2ec7248aefe10033ac
Andrei Bancioiu <andrei.bancioiu@gmail.com>
2020-12-16 10:42:51 +0200

    Sketch env.

---
+++ apidiff/profiles/env.sh
@@ -0,0 +1,20 @@
+export A=https://api.elrond.com
+export B=https://staging-api.elrond.com
+
+export PROXY_A=https://api.elrond.com
+export PROXY_B=https://staging-gateway.elrond.com
+
+export A_SUCCESSFUL_TRANSACTION=""
+export B_INVALID_TRANSACTION=""
+export C_FAILED_TRANSACTION=""
+export D_REWARDS_TRANSACTION=""
+export E_BLOCK=""
+export F_DELEGATION_ADDRESS="erd1qqqqqqqqqqqqqpgqxwakt2g7u9atsnr03gqcgmhcv38pt7mkd94q6shuwt"
+export G_MINIBLOCK_SUCCESSFUL=""
+export H_MINIBLOCK_FAILED=""
+
+export SUCCESSFUL_TRANSACTION_RECENT=""
+export FAILED_TRANSACTION_RECENT=""
+export REWARDS_TRANSACTION_RECENT=""
+
+export USER_ADDRESS=""
